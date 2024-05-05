4bc0e1010147490f1f06cf29f65b95fe4b6fc99b
doyunj <doyun.jeong@the330.co.kr>
2024-05-05 04:39:13 -0400

    feat: navigation

---
+++ ReduxPracticeApp/src/navigation/BottomTabNavigation.js
@@ -0,0 +1,33 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+
+const Tab = createBottomTabNavigator();
+
+function BottomTabNavigation() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => {
+        return {
+          headerShown: false,
+          tabBarIcon: ({ color }) => {
+            const getIconName = () => {
+              if (route.name === "History") {
+                return "time";
+              }
+
+              return "home";
+            };
+            const iconName = getIconName();
+
+            return <TabIcon iconName={iconName} iconColor={color} />;
+          },
+        };
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
+      <Tab.Screen name="History" component={HistoryListScreen}></Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+
+export default BottomTabNavigation;
